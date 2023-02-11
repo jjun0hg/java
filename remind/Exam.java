f1f2f419ce49be69589a24441e0503ec2aa68f76
jjun0hg <jjun0hg@gmail.com>
2023-02-11 16:05:39 +0900

    java

--- remind/Exam.java
+++ remind/Exam.java
@@ -1,8 +1,44 @@
-package class__;
+package re;
+
+import java.util.Scanner;
 
 public class Exam {
 
-	public static void main(String[] args) {
-		
+	Scanner sc = new Scanner(System.in);
+	private String name =null;
+	private String dap = null;
+	private char[] ox = new char[5];
+	private int score =0;
+	private final String JUNG="11111";
+	
+	Exam(){
+		System.out.println("이름입력");
+		name = sc.next();
+		System.out.println("답입력");
+		dap = sc.next();
+		this.name = name;
+		this.dap = dap;
 	}
+	public void compare() {
+		
+		for(int i= 0; i<JUNG.length();i++) {
+			if(dap.charAt(i) == JUNG.charAt(i)) {
+				ox[i] = 'o';
+				score+=20;
+			}
+			if(dap.charAt(i)!= JUNG.charAt(i)){
+				ox[i] = 'x';
+			}
+		}
+		}
+	public String getName() {
+		return name;
+	}
+	public char[] getOx() {
+		return ox;
+	}
+	public int getScore() {
+		return score;
+	}
+	
 }
--- remind/ExamMain.java
+++ remind/ExamMain.java
@@ -1,4 +1,4 @@
-package class__;
+package re;
 
 import java.util.Scanner;
 
@@ -6,10 +6,21 @@ public class ExamMain {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("인원수 입력 : ");
+		System.out.println("인원수 입력 :");
 		int cnt = sc.nextInt();
+		Exam[] e = new Exam[cnt];
+		for(int i=0; i<cnt;i++) {
+			e[i] = new Exam();
+			e[i].compare();			
+		}	
 		
+		System.out.println("이름    1 2 3 4 5 점수");
+		for(int i=0; i<cnt;i++) {
+			System.out.print(e[i].getName() + "\t");
+			System.out.print(e[i].getOx());
+			System.out.print("\t ");
+			System.out.println(e[i].getScore());
+		}
 	}
 }
 
