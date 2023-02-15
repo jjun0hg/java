10a59713292436624ad74cbb346e7d5bc39427e3
jjun0hg <jjun0hg@gmail.com>
2023-02-15 14:37:36 +0900

    java

---
+++ collection/PersonMain.java
@@ -0,0 +1,34 @@
+package collection;
+
+import java.util.ArrayList;
+
+public class PersonMain {
+
+	public ArrayList<PersonDTO> init() { // 구현부
+		PersonDTO aa = new PersonDTO("홍길동", 25);
+		PersonDTO bb = new PersonDTO("프로도", 30);
+		PersonDTO cc = new PersonDTO("라이언", 40);
+
+		ArrayList<PersonDTO> arrayList = new ArrayList<PersonDTO>();
+		arrayList.add(aa);
+		arrayList.add(bb);
+		arrayList.add(cc);
+
+		return arrayList; // 주소값가져옴
+
+	}
+
+	public static void main(String[] args) {
+		PersonMain personMain = new PersonMain();
+		ArrayList<PersonDTO> list = personMain.init();
+
+		for (PersonDTO personDTO : list) {
+			System.out.println(personDTO.getName() + "\t" + personDTO.getAge());
+		}//for
+		
+		for (PersonDTO personDTO : list) {
+			System.out.println(personDTO.getName() + "\t" + personDTO.getAge());
+		}//for
+
+	}
+}
