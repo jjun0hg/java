cf75822d463a09d3da78d469c5077ef60bc3bc6f
jjun0hg <jjun0hg@gmail.com>
2023-02-17 11:36:48 +0900

    java

--- io/ObjectMain.java
+++ io/ObjectMain.java
@@ -1,13 +1,16 @@
 package io;
 
-import java.io.FileNotFoundException;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 public class ObjectMain {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
 		PersonDTO dto = new  PersonDTO("홍길동", 25, 185.3);
 		
 		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("result2.txt"));
@@ -15,7 +18,13 @@ public class ObjectMain {
 		oos.close();
 		
 		//파일의 내용을 읽어오기
+		ObjectInputStream ois = new ObjectInputStream(new FileInputStream("result2.txt"));
 		
+		PersonDTO dto2 = (PersonDTO)ois.readObject(); //자식 = (자식)부모
+		System.out.println("이름 = " + dto2.getName());
+		System.out.println("나이 = " + dto2.getAge());
+		System.out.println("키 = " + dto2.getHeight());
+		ois.close();
 	}
 	
 }
