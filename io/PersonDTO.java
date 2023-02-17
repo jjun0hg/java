605e55ba5ee68ec2706d070df030a3fe137de04d
jjun0hg <jjun0hg@gmail.com>
2023-02-17 11:19:34 +0900

    java

---
+++ io/PersonDTO.java
@@ -0,0 +1,41 @@
+package io;
+
+import java.io.Serializable;
+
+public class PersonDTO implements Serializable{
+
+	private String name;
+	private int age;
+	private double height;
+
+	public PersonDTO(String name, int age, double height) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.height = height;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
+}
